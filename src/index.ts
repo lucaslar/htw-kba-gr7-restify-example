3c8d0d49c46bf815d0f9419a08cf850aafcc1265
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-07 11:34:06 +0200

    Initialized Restify server.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import restify from 'restify';
+
+const [host, port] = ['0.0.0.0', 8080];
+const server = restify.createServer();
+
+server.listen(port, host, () => {
+    console.log(`Running on ${host}:${port}`)
+
+    server.get('/', function (req, res, next) {
+        res.send({ message: 'Hello World!' });
+        next();
+    });
+});
