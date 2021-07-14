1a621e9984a2f6cf886cd17100d1323a6b600f85
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-14 22:13:15 +0200

    Implemented auth-based search routes.

--- src/controllers/laureate-colleague.controller.ts
+++ src/controllers/laureate-colleague.controller.ts
@@ -1,34 +1,68 @@
 import { Request, Response } from 'restify';
+import FakeDb from '../helpers/fake-db';
+
+const validQueryParams = [
+    'id',
+    'firstname',
+    'surname',
+    'born',
+    'died',
+    'bornCountry',
+    'bornCountryCode',
+    'bornCity',
+    'diedCountry',
+    'diedCountryCode',
+    'diedCity',
+    'gender',
+];
 
 export const getColleagueData = (req: Request, res: Response): void => {
-    const validQueryParams = [
-        'id',
-        'firstname',
-        'surname',
-        'born',
-        'died',
-        'bornCountry',
-        'bornCountryCode',
-        'bornCity',
-        'diedCountry',
-        'diedCountryCode',
-        'diedCity',
-        'gender',
-    ];
-
     const queryParams = Object.keys(req.query);
     const ignoredQueryParams = queryParams.filter(
         (p) => !validQueryParams.includes(p)
     );
+    const acceptedQueryParams = queryParams.filter(
+        (p) => !ignoredQueryParams.includes(p)
+    ) as (
+        | 'id'
+        | 'firstname'
+        | 'surname'
+        | 'born'
+        | 'died'
+        | 'bornCountry'
+        | 'bornCountryCode'
+        | 'bornCity'
+        | 'diedCountry'
+        | 'diedCountryCode'
+        | 'diedCity'
+        | 'gender'
+    )[];
 
-    if (
-        !queryParams.length ||
-        ignoredQueryParams.length === queryParams.length
-    ) {
+    if (!acceptedQueryParams.length) {
         const message =
             'No or no valid query param to identify your colleague by! Check the list of valid query params';
         res.send(400, { message, validQueryParams });
-    } else res.send({ ignoredQueryParams }); // TODO: Add data
+    } else {
+        const hits = FakeDb.laureates.filter((l) =>
+            acceptedQueryParams.every((p) => {
+                if (typeof l[p] === 'string') {
+                    return (
+                        (l[p] as string)?.toLowerCase() ===
+                        req.query[p].toLowerCase()
+                    );
+                } else if (l[p] instanceof Date) {
+                    return (
+                        new Date(req.query[p]).getTime() ===
+                        (l[p] as Date).getTime()
+                    );
+                } else if (l[p] === undefined) return !req.query[p];
+                else {
+                    return l[p] === +req.query[p];
+                }
+            })
+        );
+        res.send({ ignoredQueryParams, hits });
+    }
 };
 
 export const getOwnData = (req: Request, res: Response): void => {}; // TODO implement
